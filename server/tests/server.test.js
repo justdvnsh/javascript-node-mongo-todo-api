3c3dbb5328eab930e2b396de5993abe1bd398564
Divyansh Dwivedi <justdvnsh2208@gmail.com>
2018-03-12 20:33:39 +0530

    TEST for DELETE /users/me/token LOGOUT

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -290,3 +290,22 @@ describe('POST /users/login', () => {
       })
   })
 })
+
+describe('DELETE /users/me/token', () => {
+  it('should remove auth token on logout', (done) => {
+    request(app)
+      .delete('/users/me/token')
+      .set('x-auth', users[0].tokens[0].token)
+      .expect(200)
+      .end((err, res) => {
+        if (err) {
+          return err
+        }
+
+        Users.findById(users[0]._id).then((user) => {
+          expect(user.tokens.length).toEqual(0)
+          done();
+        }).catch((e) => done(e))
+      })
+  })
+})
